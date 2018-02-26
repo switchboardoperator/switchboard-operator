cc9beb15fa1238b1266beee6322793b888d88576
Genar Trias Ortiz <genar@acs.li>
2018-02-26 18:30:17 +0100

    Trying to get better logs, refs #7

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,20 @@
-module.exports.logger = require('winston')
+const winston = require('winston')
+
+const Logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: () => {
+        return Date.now()
+      },
+      formatter: (options) => {
+        return {
+          time: options.timestamp(),
+          level: options.level,
+          msg: undefined !== options.message ? options.message : ''
+        }
+      }
+    })
+  ]
+})
+
+module.exports.logger = Logger
