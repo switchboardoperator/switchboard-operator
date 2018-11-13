7c59e977b9842598466d3fa85563696bc00e4ce9
Alexander Arce <alexander@cirici.com>
2018-11-13 14:34:23 +0100

    Added silent in winston for testing

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -5,7 +5,8 @@ const Logger = new (winston.Logger)({
     new (winston.transports.Console)({
       timestamp: () => {
         return Date.now()
-      }
+      },
+      silent: process.env.NODE_ENV === 'test'
     })
   ]
 })
