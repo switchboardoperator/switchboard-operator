17af3be9319cb2f20198b9369c3703ee75011a7b
Alexander Arce <alexander@cirici.com>
2018-11-13 17:08:26 +0100

    Removed comments, added an expect

--- src/sbo-tester.spec.ts
+++ src/sbo-tester.spec.ts
@@ -1,21 +1,15 @@
 import * as fs from 'fs'
-import * as config from 'config'
 
 import { loadOperators } from '../built/services/OperatorsLoader'
 import ActionCreator from './worker/ActionCreator'
-import ActionExecuter from './worker/ActionExecuter'
-
-import Topology from './amqp/Topology'
 import Event from './model/Event'
-import Config from './model/Config'
-import TelegramPlugin from './worker/execution-plugins/telegram'
 
 jest.mock('./worker/execution-plugins/telegram')
+jest.mock('./worker/execution-plugins/prev2task')
 
 describe('switchboard-operator', () => {
   it('test', () => {
     try {
-      const noTestTypes = ['telegram', 'prev2task']
       const events = loadOperators()
       const rabbit: any = {
         handle: (queue, cb) => cb(),
@@ -27,46 +21,18 @@ describe('switchboard-operator', () => {
 
       events.forEach((event) => {
         const eventObj = new Event(event)
-        //console.log(json[eventObj.name].input)
-
         const actionCreator = new ActionCreator(
           rabbit,
           eventObj
         )
-        console.log(actionCreator.executeActions(json[eventObj.name].input))
-        /* eventObj.actions.forEach((action, index) => {
-          //console.log(action)
-          const result = {}
-          if (noTestTypes.indexOf(action.type) === -1) {
-            const executer = new ActionExecuter(action, rabbit, eventObj)
-            // console.log(executer.execute())
-          } else {
-            console.log(`Skipping ${action.type}...`)
-          }
 
-        }) */
-        /* const actionCreator = new ActionCreator(
-          rabbit,
-          eventObj
-        ) */
-        //console.log(actionCreator.executeActions(json))
+        actionCreator.executeActions(json[eventObj.name].input).then((results) => {
+          console.log(results)
+          expect(results).toEqual(json[eventObj.name].output)
+        }).catch((error) => {
+          console.error('THERE WAS AN ERROR!', error)
+        })
       })
-      /*
-      const mergedConfig = Object.assign(config.get('topology'), {
-        events: loadOperators()
-      })
-
-      const topology = new Topology(new Config(mergedConfig))
-      console.log(topology)
-      const filename = `${__dirname}/../test/operators-tester.json`
-      fs.accessSync(filename, fs.constants.R_OK)
-      const json = fs.readFileSync(filename)
-      const event: any = loadOperators()
-      const rabbit: any = {
-        handle: (queue, cb) => cb(),
-      }
-      const action = new ActionCreator(rabbit, event)
-      console.log(action.executeActions(json)) */
     } catch (e) {
       console.info('No operators-tester.json found')
     }
